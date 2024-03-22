54aa225177eda92d429110ed70f385d33c5b70a9
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2024-03-22 17:26:11 +0100

    wip

---
+++ MarsRover.js
@@ -0,0 +1,5 @@
+export class MarsRover {
+    move(commands) {
+        return '0,0,N';
+    }
+}
--- MarsRover.test.js
+++ MarsRover.test.js
@@ -1,15 +1,12 @@
-import {describe, it, expect} from "vitest";
-
-class MarsRover {
-    move(s) {
-        return undefined;
-    }
-}
+import {describe, expect, it} from "vitest";
+import {MarsRover} from "./MarsRover";
 
 describe('Mars Rover', () => {
     it('is at 0,0,N by default', () => {
-        let rover = new MarsRover();
+        const NO_MOVE = '';
 
-        expect(rover.move('')).toEqual('0,0,N')
+        const rover = new MarsRover();
+
+        expect(rover.move(NO_MOVE)).toEqual('0,0,N')
     })
 });
